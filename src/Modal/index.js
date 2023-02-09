c9177462e0b6841a19214e31b012989f77a2093c
selopez2704 <sebastianlopezmazo@gmail.com>
2023-02-08 23:05:16 -0500

    odal error solved

--- src/Modal/index.js
+++ src/Modal/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './Modal.css'
-// import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 
 
 function Modal({children}) {
-    return React.createPortal(
+    return ReactDOM.createPortal(
         <div className="ModalBackground">
             {children}
         </div>,
