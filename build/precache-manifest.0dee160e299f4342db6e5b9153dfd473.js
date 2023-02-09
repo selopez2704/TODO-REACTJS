473ba3ebd764bd393262dde8cf313656e15abeeb
selopez2704 <sebastianlopezmazo@gmail.com>
2023-02-08 20:28:41 -0500

    Adding build

---
+++ build/precache-manifest.0dee160e299f4342db6e5b9153dfd473.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea32cb0ee6660675dc9e",
+    "url": "/selopez2704/TODO-REACTJS-V01/static/css/main.26092637.chunk.css"
+  },
+  {
+    "revision": "ea32cb0ee6660675dc9e",
+    "url": "/selopez2704/TODO-REACTJS-V01/static/js/main.ea32cb0e.chunk.js"
+  },
+  {
+    "revision": "1dee63bc1a77cc940b2c",
+    "url": "/selopez2704/TODO-REACTJS-V01/static/js/1.1dee63bc.chunk.js"
+  },
+  {
+    "revision": "2b9e40285a1812879a48",
+    "url": "/selopez2704/TODO-REACTJS-V01/static/js/runtime~main.2b9e4028.js"
+  },
+  {
+    "revision": "46ded21a10f5d8a0b69fee185e67a852",
+    "url": "/selopez2704/TODO-REACTJS-V01/index.html"
+  }
+];
