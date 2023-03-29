432235f6521b2539efa9f8b6d6081acdadde9fa3
selopez2704 <sebastianlopezmazo@gmail.com>
2023-03-28 19:12:35 -0500

    Updates

---
+++ precache-manifest.949927cacdd12eb08f5ea4536bb488b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c3d7cf9d702bcddab07",
+    "url": "/TODO-REACTJS/static/css/main.347188e4.chunk.css"
+  },
+  {
+    "revision": "5c3d7cf9d702bcddab07",
+    "url": "/TODO-REACTJS/static/js/main.5c3d7cf9.chunk.js"
+  },
+  {
+    "revision": "47465028641116f10938",
+    "url": "/TODO-REACTJS/static/js/1.47465028.chunk.js"
+  },
+  {
+    "revision": "b12fd1eb3d22435770a4",
+    "url": "/TODO-REACTJS/static/js/runtime~main.b12fd1eb.js"
+  },
+  {
+    "revision": "928ea7e562ed8d88599e454f786c2a96",
+    "url": "/TODO-REACTJS/index.html"
+  }
+];
